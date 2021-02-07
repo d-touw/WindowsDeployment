b5585b1be445508d69b524b3658f7167fff187be
Dustin <dustin.touw@outlook.com>
2021-02-07 20:31:22 +0100

    modified:   Get-Scripts-Without-Git.ps1

--- Get-Scripts-Without-Git.ps1
+++ Get-Scripts-Without-Git.ps1
@@ -1,5 +1,7 @@
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/d-touw/WindowsDeployment/main/2020-DeCrapify.ps1" -OutFile 2020-DeCrapify.ps1
-New-Item -ItemType folder -Name lib
+New-Item -ItemType "Directory" -Name UncrapifyWindows
+cd UncrapifyWindows
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/d-touw/WindowsDeployment/main/DecrapifyWindows.ps1" -OutFile DecrapifyWindows.ps1
+New-Item -ItemType "Directory" -Name lib
 cd lib
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/d-touw/WindowsDeployment/main/lib/New-FolderForced.psm1" -OutFile New-FolderForced.psm1
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/d-touw/WindowsDeployment/main/lib/take-own.psm1" -OutFile take-own.psm1
