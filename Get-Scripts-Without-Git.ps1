2aa7b4c25950a55e8fa67d2dfff7d8210ad6b317
Dustin Touw <dustin.touw@outlook.com>
2021-02-07 21:28:39 +0100

    another yeeting fix

--- Get-Scripts-Without-Git.ps1
+++ Get-Scripts-Without-Git.ps1
@@ -5,3 +5,4 @@ New-Item -ItemType "Directory" -Name lib
 cd lib
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/d-touw/WindowsDeployment/Testing/lib/New-FolderForced.psm1" -OutFile New-FolderForced.psm1
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/d-touw/WindowsDeployment/Testing/lib/take-own.psm1" -OutFile take-own.psm1
+cd ..
