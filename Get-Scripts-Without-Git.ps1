a7223e815a82c925b2f66fe8ea7d0293f84b5c6b
Dustin Touw <dustin.touw@outlook.com>
2021-02-07 21:24:50 +0100

    Get from Testing

--- Get-Scripts-Without-Git.ps1
+++ Get-Scripts-Without-Git.ps1
@@ -1,7 +1,7 @@
 New-Item -ItemType "Directory" -Name DecrapifyWindows
 cd DecrapifyWindows
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/d-touw/WindowsDeployment/main/DecrapifyWindows.ps1" -OutFile DecrapifyWindows.ps1
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/d-touw/WindowsDeployment/Testing/DecrapifyWindows.ps1" -OutFile DecrapifyWindows.ps1
 New-Item -ItemType "Directory" -Name lib
 cd lib
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/d-touw/WindowsDeployment/main/lib/New-FolderForced.psm1" -OutFile New-FolderForced.psm1
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/d-touw/WindowsDeployment/main/lib/take-own.psm1" -OutFile take-own.psm1
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/d-touw/WindowsDeployment/Testing/lib/New-FolderForced.psm1" -OutFile New-FolderForced.psm1
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/d-touw/WindowsDeployment/Testing/lib/take-own.psm1" -OutFile take-own.psm1
